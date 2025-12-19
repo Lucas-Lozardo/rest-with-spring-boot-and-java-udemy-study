b5cb09d2df85c7ba3ae9d3051e6b1923fda34ae5
Lucas Lozardo <lucas_losardo@hotmail.com>
2025-12-19 20:34:10 -0300

    Implementado os métodos para subtração, multiplicação, divisão, média e raíz quadrada na classe Controller conforme desafio do curso

--- src/main/java/br/com/estudos/udemy/rest_with_spring_boot_and_java/controllers/MathController.java
+++ src/main/java/br/com/estudos/udemy/rest_with_spring_boot_and_java/controllers/MathController.java
@@ -32,4 +32,66 @@ public class MathController {
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(number);
     }
+
+    //http://localhost:8080/math/subtraction/3/5
+    @RequestMapping("/substraction/{numberOne}/{numberTwo}")
+    public Double substraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    //http://localhost:8080/math/multiplication/3/5
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+
+    }
+
+    //http://localhost:8080/math/division/3/5
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        if (numberTwo.equals("0")){
+            throw new UnsupportedMathOperationException("Please set a numeric value different for 0!");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    //http://localhost:8080/math/mean/3/5
+    @RequestMapping("/mean/{numberOne}/{numberTwo}")
+    public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo){
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    //http://localhost:8080/math/squareRoot/81
+    @RequestMapping("/squareroot/{number}")
+    public Double squareRoot(@PathVariable("number") String number){
+
+        if (!isNumeric(number)){
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        }
+
+        var numberConvertedtoDouble = convertToDouble(number);
+
+        if (numberConvertedtoDouble < 0){
+            throw new UnsupportedMathOperationException("It will not be allowed calculate the square root of negative number!");
+        }
+
+        return Math.sqrt(numberConvertedtoDouble);
+    }
 }
