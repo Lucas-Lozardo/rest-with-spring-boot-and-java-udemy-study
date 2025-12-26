ce111ebd322ff22f7ac0a8b211e7002cf7d4e569
Lucas Lozardo <lucas_losardo@hotmail.com>
2025-12-26 12:39:57 -0300

    refactor: PersonController and application.yml

--- src/main/java/br/com/estudos/udemy/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ src/main/java/br/com/estudos/udemy/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.com.estudos.udemy.rest_with_spring_boot_and_java.services.PersonServic
 import br.com.estudos.udemy.rest_with_spring_boot_and_java.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,29 +17,32 @@ public class PersonController {
     private PersonServices service;
     //private PersonServices service = new PersonServices();
 
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return service.findAll();
+    }
+
     //@GetMapping
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)    //method = RequestMethod.GET == @GetMapping    MediaType do org.springframework.http.MediaType
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)    //method = RequestMethod.GET == @GetMapping    MediaType do org.springframework.http.MediaType
     public Person findById(@PathVariable("id") Long id){
         return service.findById(id);
     }
     //@GetMapping
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)    //method = RequestMethod.POST == @PostMapping    MediaType do org.springframework.http.MediaType
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)    //method = RequestMethod.POST == @PostMapping    MediaType do org.springframework.http.MediaType
     public Person create(@RequestBody Person person){
         return service.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person){
         return service.update(person);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE)
-        public void delete(@PathVariable Long id){
+    @DeleteMapping
+        public ResponseEntity<?> delete(@PathVariable Long id){
         service.delete(id);
-    }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
-        return service.findAll();
+        return ResponseEntity.noContent().build();
     }
 }
