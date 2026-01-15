1a8eaf41420fb6004f97639dc5340ad50f1e6ac4
Lucas Lozardo <lucas_losardo@hotmail.com>
2026-01-15 20:26:45 -0300

    refactor: Swagger move to interface for a better maintenance

--- src/main/java/br/com/estudos/udemy/rest_with_spring_boot_and_java/controllers/PersonController.java
+++ src/main/java/br/com/estudos/udemy/rest_with_spring_boot_and_java/controllers/PersonController.java
@@ -1,13 +1,9 @@
 package br.com.estudos.udemy.rest_with_spring_boot_and_java.controllers;
 
+import br.com.estudos.udemy.rest_with_spring_boot_and_java.controllers.docs.PersonControllerDocs;
 import br.com.estudos.udemy.rest_with_spring_boot_and_java.data.dto.v1.PersonDTO;
 import br.com.estudos.udemy.rest_with_spring_boot_and_java.data.dto.v2.PersonDTOV2;
 import br.com.estudos.udemy.rest_with_spring_boot_and_java.services.PersonServices;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -19,102 +15,58 @@ import java.util.List;
 @RestController
 @RequestMapping("api/person")
 @Tag(name = "People", description = "Endpoints for Managing People")
-public class PersonController {
+public class PersonController implements PersonControllerDocs {
 
     @Autowired
     private PersonServices service;
     //private PersonServices service = new PersonServices();
 
 
-    @GetMapping(value = "/v1", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
-    @Operation(summary = "Find All People", description = "Find all People", tags = {"People"}, responses = {
-            @ApiResponse(description = "Success", responseCode = "200", content = {
-                    @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class))
-                    )
-            }),
-            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-    })
+    @GetMapping(value = "/v1", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override       /// para o implements
     public List<PersonDTO> findAll(){
         return service.findAll();
     }
 
-    //@GetMapping
-    @GetMapping(value = "/v1/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })    //method = RequestMethod.GET == @GetMapping    MediaType do org.springframework.http.MediaType
-    @Operation(summary = "Find a Person", description = "Find a Specific Person by your ID", tags = {"People"}, responses = {
-            @ApiResponse(description = "Success", responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = PersonDTO.class))
-            ),
-            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-    })
+
+    @GetMapping(value = "/v1/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO findById(@PathVariable("id") Long id){
         return service.findById(id);
     }
 
-    //@GetMapping
-    @Operation(summary = "Adds a new Person", description = "Adds a new Person", tags = {"People"}, responses = {
-            @ApiResponse(description = "Success", responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = PersonDTO.class))
-            ),
-            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-    })
+
     @PostMapping(value = "/v1",
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })    //method = RequestMethod.POST == @PostMapping    MediaType do org.springframework.http.MediaType
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO create(@RequestBody PersonDTO person){
         return service.create(person);
     }
 
+
     //VERSION 2
     @PostMapping(value = "/v2",
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTOV2 createV2(@RequestBody PersonDTOV2 person){
         return service.createV2(person);
     }
 
-    @Operation(summary = "Update a Person", description = "Update a Specific Person by your ID", tags = {"People"}, responses = {
-            @ApiResponse(description = "Success", responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = PersonDTO.class))
-            ),
-            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-    })
+
     @PutMapping(value = "/v1",
-            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public PersonDTO update(@RequestBody PersonDTO person){
         return service.update(person);
     }
 
-    @Operation(summary = "Delete a Person", description = "Delete a Specific Person by your ID", tags = {"People"}, responses = {
-            @ApiResponse(description = "Success", responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = PersonDTO.class))
-            ),
-            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
-            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
-            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
-    })
+
     @DeleteMapping(value = "/v1")
-        public ResponseEntity<?> delete(@PathVariable Long id){
+    @Override
+    public ResponseEntity<?> delete(@PathVariable Long id){
         service.delete(id);
 
         return ResponseEntity.noContent().build();
